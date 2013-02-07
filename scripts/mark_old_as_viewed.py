c5f412de1a559e8965314d5222ac86521109f5fe
ClementNotin <clement@notin.org>
2013-02-08 00:29:53 +0100

    New: script pour marquer comme lu chez tout le monde les vieux apparts

---
+++ scripts/mark_old_as_viewed.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from lebonsite import  db
+from entities import *
+from datetime import datetime
+
+print "Go"
+
+users = User.query.all()
+old_apparts = Appartement.query.filter(Appartement.date < datetime(2013, 02, 07, 21, 0)).order_by(Appartement.date)
+
+for appart in old_apparts:
+    for user in users:
+        appart.seen_by(user)
+
+db.session.commit()
+print "End"
